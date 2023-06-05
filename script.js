e3d2c4c276da0a520b39b55dcabe13319091474c
Nikhil Sahu <heynikhil07@gmail.com>
2023-06-05 22:31:45 +0530

    Lecture: #4 Learned about looping forEach methods with maps and sets

--- script.js
+++ script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-    ['USD', 'United States dollar'],
-    ['EUR', 'Euro'],
-    ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -157,4 +157,28 @@ const currencies = new Map([
 // //2: function(-400)
 // //...
 // // continue, break don't work in forEach
+
+
+// //////////////////////// forEach With Maps and Sets //////////////////////
+
+// // MAP
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function (key, value, map) {
+//     console.log(`${key}: ${value}`);
+
+// })
+
+// // SET
+// const currenciesUnique = new Set(['USD', 'INR', 'USD', 'EUR', 'INR']);
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function (value, _value, set) {
+//     console.log(`${value}: ${_value}`);
+
+// })
 
